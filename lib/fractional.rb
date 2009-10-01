a3b140a95b5b8056c269f7063e0b515b041980f3
Chris O'Sullivan <thechrisoshow@gmail.com>
2009-10-01 15:03:59 +0100

    Added basic functionality for parsing between strings and fractions.  Also updated the readme

---
+++ lib/fractional.rb
@@ -0,0 +1,59 @@
+module Fractional
+
+  def self.to_f(value)
+    if mixed_fraction?(value)
+      whole, numerator, denominator = value.scan(/(\d*)\s(\d+)\/(\d+)/).flatten
+      (numerator.to_f / denominator.to_f) + whole.to_f
+    elsif single_fraction?(value)
+      numerator, denominator = value.split("/")
+      numerator.to_f / denominator.to_f
+    else
+      value.to_f
+    end
+  end
+
+  def self.to_s(value)
+    whole_number = value.to_f.truncate.to_i
+    
+    if whole_number == 0
+      float_to_rational(value.to_f).to_s  
+    else
+      decimal_point_value = get_decimal_point_value(value.to_f)      
+      whole_number.to_s + " " + float_to_rational(decimal_point_value).to_s
+    end 
+  end
+
+  private
+    
+  def self.fraction?(value)
+    value.to_s.count('/') == 1
+  end
+
+  def self.single_fraction?(value)
+    fraction?(value) and !mixed_fraction?(value)
+  end
+
+  def self.mixed_fraction?(value)
+    fraction?(value) and value.match(/\d*\s+\d+\/\d+/)
+  end
+  
+  def self.get_decimal_point_value(value)
+    value - value.truncate
+  end
+
+  def self.float_to_rational(value)
+    if value.nan? 
+      return Rational(0,0) # Div by zero error 
+    elsif value.infinite? 
+      return Rational(value<0 ? -1 : 1,0) # Div by zero error 
+    end 
+    s,e,f = [value].pack("G").unpack("B*").first.unpack("AA11A52") 
+    s = (-1)**s.to_i 
+    e = e.to_i(2) 
+    if e.nonzero? and e<2047
+      Rational(s)* Rational(2)**(e-1023)*Rational("1#{f}".to_i(2),0x10000000000000) 
+    elsif e.zero? 
+      Rational(s)* Rational(2)**(-1024)*Rational("0#{f}".to_i(2),0x10000000000000) 
+    end 
+  end 
+end
--- spec/fractional_spec.rb
+++ spec/fractional_spec.rb
@@ -1,7 +1,41 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Fractional" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Fractional", "to_f" do
+  
+  it "should parse '1/2' to 0.5" do
+    Fractional.to_f('1/2').should == 0.5
   end
+  
+  it "should parse '1 2/4' to 1.5" do
+    Fractional.to_f('1 2/4').should == 1.5
+  end
+  
+  it "should parse '1.5' to 1.5" do
+    Fractional.to_f('1.5').should == 1.5    
+  end
+
+  it "should parse 1.5 to 1.5" do
+    Fractional.to_f(1.5).should == 1.5    
+  end
+  
+  it "should parse '1100 7/8' to 1100.875" do
+    Fractional.to_f("1100 7/8").should == 1100.875
+  end
+  
+end
+
+describe "Fractional", "to_s" do
+  
+  it "should return 0.5 as '1/2'" do
+    Fractional.to_s(0.5).should == '1/2'
+  end
+  
+  it "should return 1.5 as '1 1/2'" do
+    Fractional.to_s(1.5).should == '1 1/2'
+  end
+  
+  it "should parse 1100.875 to '1100 7/8'" do
+    Fractional.to_s(1100.875).should == "1100 7/8"
+  end
+  
 end
